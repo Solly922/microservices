ff9f994c0807374e6bbdf40ccfa9e3c8181002e6
Solomon Schuster <90484793+Solly922@users.noreply.github.com>
2023-10-10 15:05:12 -0700

    adds mail service

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "80"
 
 func main() {
 
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("Starting mail service on port", webPort)
 
@@ -27,4 +32,21 @@ func main() {
 		log.Panic(err)
 		return
 	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+
+	return m
 }
